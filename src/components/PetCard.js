90e5e86afdef7db91ff026821758540d21009374
Mikey <46366395+mikikiv@users.noreply.github.com>
2022-03-14 22:23:10 -0700

    More of the first react component

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -3,8 +3,20 @@ import React from 'react';
 
 export default function PetCard({ pet, imagePath, successMessage, children }) {
     return <div
-    style={{ backgroundColor: "red" }}>
-        <span><h1>{pet}</h1><img src="{{ imagePath }}"/></span>
+        style={{
+            backgroundColor: "red",
+            height: 'auto',
+            width: '100%'
+        }}>
+        <div>
+            <img
+            style={{
+                height: '200px',
+                float: 'right',
+                padding: '2%'
+            }} src={imagePath} />
+        </div>
+            <h1>{pet}</h1>
         <div>{children}</div>
         <div>
             <button>{successMessage}</button>
