39177b91351c612de4455321ed3092c90e580410
Mikey Villavicencio <villavicem@gmail.com>
2022-03-24 18:23:38 -0700

    maybe management page for content writing someday

---
+++ src/pages/management.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import PetCard from '../components/PetCard';
+
+
+
+
+export default function Home() {
+  return (
+    <Layout
+      title={`Site management`}
+      description="Manage your pets from here">
+      <main>
+        
+
+      </main>
+    </Layout>
+  );
+}
