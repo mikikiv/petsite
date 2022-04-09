823fbb89ed8cd59fcf2e3a929ae08c39fcd58589
Mikey Villavicencio <villavicem@gmail.com>
2022-04-09 09:33:26 -0700

    init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/mikikiv/petsite/edit/staging/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/mikikiv/petsite/edit/staging/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
---
+++ pages/LeftSidebar.js
@@ -0,0 +1,4 @@
+const { I } = inject();
+
+module.exports = {
+  FirstLevel: '.theme-doc-sidebar-item-category-level-1'}
